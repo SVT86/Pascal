51b8ec4520a07644d2bea957a79c2d1fc031c29d
Seba <sebatl2070@gmail.com>
2021-04-24 12:40:32 -0300

    Versión 1.1.0

--- begin.pas
+++ begin.pas
@@ -1,9 +1,12 @@
 
 Program beginProgram;
 
-  Var 
-    nombre: String;
-  Begin
-    nombre := 'Sebastián';
-    writeln(nombre);
-  End.
+Var 
+  nombre: String;
+  x: integer;
+Begin
+  nombre := 'Sebastián';
+  writeln(nombre);
+  For x:=1 To 200 Do
+    writeln(x);
+End.
