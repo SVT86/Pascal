487df6888a6de0cf86c4d4e15e4584d1487a2165
SebaTL70 <45188987+SebaTL70@users.noreply.github.com>
2021-04-24 23:48:29 -0300

    Update begin.pas

--- begin.pas
+++ begin.pas
@@ -1,12 +1,17 @@
 
 Program beginProgram;
+uses
+  crt;
 
 Var 
   nombre: String;
   x: integer;
 Begin
+  ClrScr();
   nombre := 'Sebastián';
   writeln(nombre);
   For x:=1 To 10 Do
     writeln('2 x ',x,' = ',(2*x));
+  writeln('Presione una tecla para continuar ...');
+  ReadKey();
 End.
